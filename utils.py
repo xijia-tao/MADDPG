b46ef6e37a15929a86a7a1a390c3b9c0260c6b4f
EE-LiuYunhao <liuyunhao1578@gmail.com>
2021-03-13 17:39:44 +0800

    Implement the methods

--- utils.py
+++ utils.py
@@ -1,50 +1,114 @@
-from typing import Any, Callable, List, Sequence, Tuple, Type, Union
 from collections import OrderedDict
+import copy
+from typing import Any, Callable, List, Optional, Sequence, Type, Union
 import gym
 import ma_gym as _
 import numpy as np
-from stable_baselines3.common.type_aliases import GymEnv
 import torch
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv, VecEnvIndices, VecEnvObs, VecEnvStepReturn
-from stable_baselines3.common.vec_env.util import obs_space_info
+from stable_baselines3.common.vec_env.util import copy_obs_dict, obs_space_info, dict_to_obs
 
-class MultiAgentEnvWrapper(VecEnv): 
-    """
+class VectorizedMultiAgentEnvWrapper(VecEnv): 
+    """ The wrapper for multi agent environment
+
+    This wrapping the MA env(s) into a VecEnv, s.t. it(they) can 
+    perform in the compatible way with those models defiend for
+    single agent environments. 
     """
     def __init__(self, env_fns: List[Callable[[], gym.Env]]):
+        """ Constructor
+
+        Args:
+             env_fns: a list of functions that return environments to vectorize
         """
+        self.envs = [MultiAgentEnvWrapper(fn()) for fn in env_fns]
+        env = self.envs[0]
+        VecEnv.__init__(self, len(env_fns), env.observation_space, env.action_space)
+        obs_space = env.observation_space
+        self.keys, shapes, dtypes = obs_space_info(obs_space)
+
+        self.buf_obs = OrderedDict([(k, np.zeros((self.num_envs,) + tuple(shapes[k]), dtype=dtypes[k])) for k in self.keys])
+        self.buf_dones = np.zeros((self.num_envs,), dtype=bool)
+        self.buf_rews = np.zeros((self.num_envs, 1, env.agent_num), dtype=np.float32)
+        self.buf_infos = [{} for _ in range(self.num_envs)]
+        self.actions = None
+        self.metadata = env.metadata
+
+    def _save_obs(self, env_idx: int, obs: VecEnvObs) -> None:
+        """ Save the observation of a certain ma-env to obs_buf
+
+        Args: 
+            env_idx: index of the env whose observation is to be saved
+            obs: the vectorized env observations
         """
-        pass 
+        for key in self.keys:
+            if key is None:
+                self.buf_obs[key][env_idx] = obs
+            else:
+                self.buf_obs[key][env_idx] = obs[key]
 
     def reset(self) -> VecEnvObs:
-        pass 
+        for env_idx, each_env in enumerate(self.num_envs):
+            obs = each_env.reset()
+            self._save_obs(env_idx, obs)
+        return dict_to_obs(self.observation_space, copy_obs_dict(self.buf_obs))
 
     def step_async(self, actions: np.ndarray) -> None:
-        pass 
+        # store the action
+        self.actions = actions
 
     def step_wait(self) -> VecEnvStepReturn:
-        pass
+        # wield the stored action for env.step
+        for idx, env in enumerate(self.envs):
+            obs, self.buf_rews[idx], self.buf_dones[idx], self.buf_infos[idx] = env.step(
+                self.actions[idx]
+            )
+            if self.buf_dones[idx]:
+                # save final observation where user can get it, then reset
+                self.buf_infos[idx]["terminal_observation"] = obs
+                obs = self.envs[idx].reset()
+            self._save_obs(idx, obs)
+        return (self._obs_from_buf(), np.copy(self.buf_rews), np.copy(self.buf_dones), copy.deepcopy(self.buf_infos))
 
     def close(self) -> None:
-        pass
+        for env in self.envs: env.close()
+
     def get_attr(self, attr_name: str, indices: VecEnvIndices) -> List[Any]:
-        pass
+        target_envs = self._get_target_envs(indices)
+        return [getattr(env_i, attr_name) for env_i in target_envs]
+
     def set_attr(self, attr_name: str, value: Any, indices: VecEnvIndices) -> None:
-        pass
+        target_envs = self._get_target_envs(indices)
+        for env_i in target_envs:
+            setattr(env_i, attr_name, value)
 
     def env_method(self, method_name: str, *method_args, indices: VecEnvIndices, **method_kwargs) -> List[Any]:
-        pass
+        target_envs = self._get_target_envs(indices)
+        return [getattr(env_i, method_name)(*method_args, **method_kwargs) for env_i in target_envs]
 
     def env_is_wrapped(self, wrapper_class: Type[gym.Wrapper], indices: VecEnvIndices) -> List[bool]:
-        pass
+        target_envs = self._get_target_envs(indices)
+        from stable_baselines3.common import env_util
+        return [env_util.is_wrapped(each_env, wrapper_class) for each_env in target_envs]
     
     def get_images(self) -> Sequence[np.ndarray]:
-        pass
+        return [env.render(mode="rgb_array") for env in self.envs]
 
-    def render(self, mode: str) -> Optional[np.ndarray]:
-        return super().render(mode=mode)
+    def render(self, mode: str = "human") -> Optional[np.ndarray]:
+        if self.num_envs == 1:
+            return self.envs[0].render(mode)
+        else:
+            return super().render(mode="rgb_array")
+
+    def seed(self, seed: Optional[int]) -> List[Union[None, int]]:
+        return [env.seed(seed + idx) for idx, env in enumerate(self.envs)]
+
+    def _get_target_envs(self, indices: VecEnvIndices) -> List[gym.Env]:
+        indices = self._get_indices(indices)
+        return [self.envs[i] for i in indices]
+
     
-class env_wrapper:
+class MultiAgentEnvWrapper:
     """
     A wrapper for ma-gym environments
 
@@ -63,7 +127,7 @@ class env_wrapper:
         - Convert discrete action space to continuous one. 
         - Add reward_range & metadata attributes
     """
-    def __init__(self, env: Union[str, GymEnv]):
+    def __init__(self, env: Union[str, gym.Env]):
         if isinstance(env, str):
             env = gym.make(env)
 
@@ -103,8 +167,20 @@ class env_wrapper:
             states.append(state)
         return states
 
+    def seed(self, i: int):
+        return self.env.seed(i)
+
+    def render(self, mode: str = "human"):
+        return self.env.render(mode)
+
+    def close(self):
+        return self.env.close()
+
+    def reset(self):
+        return self.env.reset()
+
     def step(self, actions):
-        actions = Tensor(actions)
+        actions = torch.Tensor(actions)
         # action is of shape(2, 1), varied within (-1.0, 1,0)
         actions_int = []
         ## with torch.no_grad:
@@ -113,5 +189,5 @@ class env_wrapper:
         states, rewards, dones, info = self.env.step(actions_int)
         # state: list of Tensor(10,), required Tensor(2,10)
         # rewards: list of int, required Tensor(1,2)
-        states = [Tensor(state) for state in states]
+        states = [torch.Tensor(state) for state in states]
         return torch.stack(states), torch.Tensor(rewards).reshape(-1, len(rewards)), any(dones), info
