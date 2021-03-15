44061e4b7f5b88e346c48172985de945812068ad
EE-LiuYunhao <liuyunhao1578@gmail.com>
2021-03-15 14:26:48 +0800

    add comments

--- env_wrapper.py
+++ env_wrapper.py
@@ -17,7 +17,7 @@ class VectorizedMultiAgentEnvWrapper(VecEnv):
     perform in the compatible way with those models defiend for
     single agent environments. 
     """
-    def __init__(self, env_fns: List[Tuple[Callable[[], Union[str, gym.Env]], Callable[[torch.Tensor], torch.Tensor]]]):
+    def __init__(self, env_fns: List[Tuple[Callable[[], Union[str, gym.Env]], Callable[[torch.Tensor], Union[torch.Tensor, np.ndarray, list]]]]):
         """ Constructor
 
         Args:
@@ -123,35 +123,35 @@ class VectorizedMultiAgentEnvWrapper(VecEnv):
 
     
     class MultiAgentEnvWrapper:
-        """
-        A wrapper for ma-gym environments
+        """ Direct wrapper for EACH multi-agent environment. 
 
-        Args:
-            env: name of the ma-gym environment (str)
-
-        - Compatible with observation space of high dimension
-        - Assume the type of observation space is gym.spaces.Box 
-        (more types can be added at a later stage)
-        - Assume discrete action space
-        (can add supports for continuous one at a later stage)
-        
-        Implemented:
-            - Addressed the attribute error with the original observation space 
-            of ma-gym by "concatenating" the first dimensions
-            - Convert discrete action space to continuous one. 
-            - Add reward_range & metadata attributes
+        We have this wrapper to ensure that each ENV outputs the multi-agent obseravation and 
+        accept the corresponding action in form of Tensors, instead of list of arrays or
+        tensors. Accordingly, the observation space and action space are converted to ensure
+        the models which are designed for single agent env will be compitable with the ma ones.
         """
-        def __init__(self, env: Union[str, gym.Env], mapper: Callable[[torch.Tensor], torch.Tensor]):
+        def __init__(self, env: Union[str, gym.Env], mapper: Callable[[torch.Tensor], Union[torch.Tensor, np.ndarray, list]] = None):
+            """ Constructor
+
+            Args:
+                env: the environment to be wrapped; either a string if the env has been registered to
+                    gym, or the gym.Env object. 
+                mapper: a function object, which maps the action outputs from the model to what can be
+                    accepted by the env object. e.g. A TD3 outputs a tensor of float32 values which
+                    vary within (-1.0, 1.0), and environment like Pone-Duel requires list of integers,
+                    whose value only be 1, 2 or 3. A mapper will be needed to resolve this gap. 
+            """
             if isinstance(env, str):
                 env = gym.make(env)
 
             self.env       = env
-            self.mapper    = mapper
+            self.mapper    = mapper if mapper is not None else lambda action: action
             self.agent_num = len(env.action_space)
 
-
-            self.observation_space_ = env.observation_space[0]
-            self.observation_space  = self._wrap_obs()
+            # NOTE: the env.observation_space is a list, the i-th element corresponding to
+            # the i-th agent's own observation space. 
+            self.observation_space_ = env.observation_space[0] # the obs space for the first agent, as the representative
+            self.observation_space  = self._wrap_obs() # the obs space for the ENTRIE multi-agent env, after wrapped
 
             self.action_space_ = env.action_space[0]
             self.action_space  = self._wrap_act()
@@ -159,16 +159,41 @@ class VectorizedMultiAgentEnvWrapper(VecEnv):
             self.reward_range = env.reward_range
             self.metadata     = env.metadata
 
-        def _wrap_obs(self):
+        def _wrap_obs(self) -> spaces.Box:
+            """ Wrap the observation space
+
+            The method must be called AFTER self.observation_space_ is set, and this method
+            prepare the self.observation_space (with no underscore as its suffix). In the
+            wrapping, one dimension will be added to the observation space as its first dimension
+            which represents the number of agents. 
+
+            Returns: 
+                An box space, whose shape is (agent num, the shape of the original
+                self.observation_space_)
+            """
             obs_shape  = self.observation_space_.shape
             obs_shape_ = [self.agent_num if i == -1 else obs_shape[i] for i in range(-1,len(obs_shape))]
             obs_shape_ = tuple(obs_shape_)
             obs_low    = np.array(self.observation_space_.low).flatten()[0]
             obs_high   = np.array(self.observation_space_.high).flatten()[0]
 
-            return gym.spaces.Box(obs_low, obs_high, obs_shape_)
+            return spaces.Box(obs_low, obs_high, obs_shape_)
 
-        def _wrap_act(self):
+        def _wrap_act(self) -> spaces.Box:
+            """ Wrap the action space. 
+
+            Similar to self._wrap_obs. This method must be called AFTER self.action_space_ i set. 
+            Preparing for the self.action_space, it first identifies the type of EACH single agent's
+            own action space, and create the acceptable action space. 
+
+            NOTE: 
+                Due to the limitation of TD3 model, we treat the discrete space as a special case of
+                Box whose shape = (1,)
+
+            Returns:
+                A box space, whose shape is (agent num, 1) if the action space is discrete, and
+                (agent num, the shape of the original self.action_space_) otherwise. 
+            """
             # calculate the action dimension
             if isinstance(self.action_space_, spaces.Discrete):
                 act_num = (1,) # Discrete => ONE
@@ -178,28 +203,64 @@ class VectorizedMultiAgentEnvWrapper(VecEnv):
                 act_num = (len(self.action_space_),) + self.action_space_[0].shape if isinstance(self.action_space_[0], spaces.Box) else (len(self.action_space_), 1) 
             else:
                 act_num = (1,) # DEFAULT
-            return gym.spaces.Box(-1, 1, (self.agent_num,)+act_num)
+            return spaces.Box(-1, 1, (self.agent_num,)+act_num)
 
-        def reset(self, **kwargs):
+        def reset(self, **kwargs) -> torch.Tensor:
+            """ Reset the environemnt
+
+            Args: 
+                kwargs: keyword arguments for the reset method of the inner environment
+            Returns: 
+                The intialized state, re-formed into a tensor the same shape as self.observation_space
+            """
             states_ = self.env.reset(kwargs)
             states = []
             for state in states_:
                 states.append(state)
-            return states
+            return torch.Tensor(states)
 
-        def seed(self, i: int):
+        def seed(self, i: int) -> None:
+            """ Set the seeds for the inner environment
+
+            Args:
+                i: the int for seeding the env
+            """
             return self.env.seed(i)
 
-        def render(self, mode: str = "human"):
+        def render(self, mode: str = "human") -> Any:
+            """ Render the current state of environment
+            
+            Args: mode: one of the followings (NOTE some ENV does not support all of them)
+                - `human`: render to the current display or terminal and
+                return nothing. Usually for human consumption.
+                - `rgb_array`: Return an numpy.ndarray with shape (x, y, 3),
+                representing RGB values for an x-by-y pixel image, suitable
+                for turning into a video.
+                - `ansi`: Return a string (str) or StringIO.StringIO containing a
+                terminal-style text representation. The text can include newlines
+                and ANSI escape sequences (e.g. for colors).
+
+            Returns:
+                The rendering result, usually an np.ndarray
+            """
             return self.env.render(mode)
 
-        def close(self):
+        def close(self) -> None:
+            """ Close the env
+            """
             return self.env.close()
 
-        def reset(self):
-            return self.env.reset()
+        def step(self, actions: Union[torch.Tensor, np.ndarray, list]) -> Tuple[torch.Tensor, torch.Tensor, bool, Any]:
+            """ Execute the actions and return the results
 
-        def step(self, actions):
+            Args: 
+                actions: descibing the multi-agent actions. Note the first dimension must represend the number of agents
+            Returns: 
+                - A `torch.Tensor` for states, whose shape is (agent num, original state shape for EACH agent)
+                - A `torch.Tensor` for rewards, of shape (1, agent num)
+                - A `bool`, identifying whether an episode is DONE
+                - Anything that stores the extra info 
+            """
             if not isinstance(actions, torch.Tensor):
                 if isinstance(actions, np.ndarray):
                     actions = torch.from_numpy(actions)
@@ -207,9 +268,8 @@ class VectorizedMultiAgentEnvWrapper(VecEnv):
                     actions = torch.Tensor(actions)
             # action is of shape(2, 1), varied within (-1.0, 1,0)
 
-            actions_int = []
-            ## with torch.no_grad:
-            actions_int.extend(self.mapper(actions))
+            with torch.no_grad:
+                actions_int = self.mapper(actions)
 
             states, rewards, dones, info = self.env.step(actions_int)
             # state: list of Tensor(10,), required Tensor(2,10)
